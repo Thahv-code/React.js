02a6d7f2e87fbf1b9fc3038370501506de0c3c67
Thahv-code <vuthanhnguyen946@gmail.com>
2025-08-25 22:25:04 +0700

    update

--- Session15/vite-project/src/compoment/bt1.tsx
+++ Session15/vite-project/src/compoment/bt1.tsx
@@ -1,9 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export default class bt1 extends Component {
+interface State {
+  email: string;
+}
+
+class EmailForm extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      email: '',
+    };
+  }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ email: e.target.value });
+  };
+
+  handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ email: this.state.email });
+  };
+
   render() {
     return (
-      <div>bt1</div>
-    )
+      <>
+        <h2>Form</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>Email</label>
+          <input
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </>
+    );
   }
 }
+
+export default EmailForm;
---
+++ Session15/vite-project/src/compoment/bt2.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class bt2 extends Component {
+
+const CorlorForm: React.
+
+  render() {
+    return (
+      
+    )
+  }
+}
