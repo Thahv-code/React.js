ed2e6139ef27492a17b08c1090d0c240b9cffaab
Thahv-code <vuthanhnguyen946@gmail.com>
2025-09-04 11:07:29 +0700

    update

---
+++ Session21/vite-project/src/assets/compoment/bt4.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function bt4() {
+  return (
+        <div className="flex flex-col gap-4">
+      <button className="bg-[#0ea5e9] text-white px-6 py-2 rounded-full transition hover:bg-[#0369a1]">
+        Submit
+      </button>
+    </div>
+
+  )
+}
